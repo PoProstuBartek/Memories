fc7d41d8896f2b1cbf6127d64736b6ab194b6c22
Bartłomiej Szczotkowski <szczotkowski.b@gmail.com>
2022-05-21 23:23:44 +0200

    Deployment settings

--- server/index.js
+++ server/index.js
@@ -21,5 +21,5 @@ app.get('/', (request, response) => {
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server runing on port: ${PORT}`)))
-  .catch((error) => console.log(error.message));
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
