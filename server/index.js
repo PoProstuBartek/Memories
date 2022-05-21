ce268ef1b9cb819a1c67b1abe9caddff733e4af3
Bartłomiej Szczotkowski <szczotkowski.b@gmail.com>
2022-05-21 18:34:04 +0200

    deployment settings

--- server/index.js
+++ server/index.js
@@ -14,6 +14,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+app.get('/', (request, response) => {
+  response.send('Hello to memories API');
+})
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
